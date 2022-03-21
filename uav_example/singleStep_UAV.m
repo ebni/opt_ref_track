5a028b81a8e577fc9c009614a39d40b50f9a4875
jacobhiggins <jdh4je@virginia.edu>
2022-03-21 09:37:20 -0400

    update

--- uav_example/singleStep_UAV.m
+++ uav_example/singleStep_UAV.m
@@ -9,16 +9,16 @@ addpath('..');
 
 %% Parameters
 tau = 0.1;   % period of references
-N = 10;      % number of intervals
+N = 20;      % number of intervals
 t0 = 0;
-x0 = [0; 1; 0; 0]; % initial sampled state
+x0 = [0; 0; 0; 0]; % initial sampled state
 
 %% Dynamics 
 [sysd,sysc] = uav_dynamics();
 
 %% Trajectory to be followed
-yTildex = @(t) sqrt(t);
-yTildey = @(t) t-t+1;
+yTildex = @(t) sin(6*t.^2)+t;
+yTildey = @(t) t+0.25;
 
 %% Get optimal references (waypoints) in both x and y directions
 [r_opt, y_opt, Q_final, lin_final] = opt_refs_uav(sysd, x0, t0, N, tau, yTildex, yTildey);
@@ -26,5 +26,12 @@ yTildey = @(t) t-t+1;
 %% Plot desired trajectory, optimal references and resulting trajectory
 close all;
 figure(1); hold on; grid on;
-plot(yTildex(y_opt.x.ts),yTildey(y_opt.y.ts));
-plot(y_opt.x.ys,y_opt.y.ys);
+plot(yTildex(y_opt.x.ts),yTildey(y_opt.y.ts),...
+    'DisplayName','Reference Trajectory',...
+    'LineWidth',2);
+plot(y_opt.x.ys,y_opt.y.ys,...
+    'DisplayName','Actual Trajectory',...
+    'LineWidth',2);
+% scatter(r_opt.x,r_opt.y);
+xlabel("X Position (m)"); ylabel("Y Position (m)");
+legend('Location','southeast');
